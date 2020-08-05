dec6f3d9095913344f4125c49c87a7d40583b942
Sorin <jordache_sor@yahoo.com>
2020-08-05 17:22:02 +0300

    added utility prefab

---
+++ Assets/Scripts/UtilityClick.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class UtilityClick : MonoBehaviour
+{
+    public TextMesh nameMesh;
+    public TextMesh costMesh;
+
+    public SpriteRenderer backgroundRenderer;
+
+    private void OnMouseDown()
+    {
+        nameMesh.text = "King Cross\nStation";
+        costMesh.text = "$150";
+
+        backgroundRenderer.color = Color.white;
+    }
+}
