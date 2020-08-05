868b66a43c8c9c39884137aa88af83e4af7a2a21
Sorin <jordache_sor@yahoo.com>
2020-08-05 17:27:10 +0300

    added chance prefab

---
+++ Assets/Scripts/ChanceClick.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChanceClick : MonoBehaviour
+{
+    public TextMesh nameMesh;
+
+    public SpriteRenderer backgroundRenderer;
+
+    private void OnMouseDown()
+    {
+        nameMesh.text = "Community\nChest";
+
+        backgroundRenderer.color = Color.white;
+    }
+}
