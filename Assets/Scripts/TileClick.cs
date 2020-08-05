3939f807740129ed1066d30f75641db0da26f8f7
Sorin <jordache_sor@yahoo.com>
2020-08-05 11:19:19 +0300

    added click interaction for the game tiles

---
+++ Assets/Scripts/TileClick.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class TileClick : MonoBehaviour
+{
+    public TextMesh nameMesh;
+    public TextMesh costMesh;
+
+    public SpriteRenderer topRenderer;
+
+    private void OnMouseDown()
+    {
+        nameMesh.text = "Park Place";
+        costMesh.text = "$200";
+
+        topRenderer.color = Color.blue;
+    }
+
+}
