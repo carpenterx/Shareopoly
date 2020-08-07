dacd0a07a7ecfffe75a9d2eb21537a34fdf13403
Sorin <jordache_sor@yahoo.com>
2020-08-07 17:57:34 +0300

    added dice rolls (fixes #3)

---
+++ Assets/Scripts/DiceImage.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DiceImage : MonoBehaviour
+{
+    public List<Sprite> faceSprites;
+
+    private Image image;
+
+    public int Roll()
+    {
+        image = GetComponent<Image>();
+        int dieIndex = Random.Range(0, faceSprites.Count);
+        image.sprite = faceSprites[dieIndex];
+        Quaternion randomRotation = Quaternion.Euler(0, 0, Random.Range(0, 360));
+        transform.rotation = randomRotation;
+        return dieIndex + 1;
+    }
+}
---
+++ Assets/Scripts/DiceManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class DiceManager : MonoBehaviour
+{
+    public List<DiceImage> diceList;
+
+    public void RollDice()
+    {
+        int totalRoll = 0;
+        foreach (DiceImage dice in diceList)
+        {
+            totalRoll += dice.Roll();
+        }
+        Debug.Log("Rolled: " + totalRoll);
+    }
+}
