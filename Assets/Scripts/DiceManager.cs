d12db4748b182bf97e77bd14d24780aed1d51444
Sorin <jordache_sor@yahoo.com>
2020-08-08 13:34:49 +0300

    added player movement script based on dice rolls

--- Assets/Scripts/DiceManager.cs
+++ Assets/Scripts/DiceManager.cs
@@ -5,6 +5,31 @@ public class DiceManager : MonoBehaviour
 {
     public List<DiceImage> diceList;
 
+    public GameObject player;
+    public GameObject tilesHolder;
+
+    private List<Transform> tileList = new List<Transform>();
+    private int curentTileIndex = 0;
+
+    void Start()
+    {
+        GenerateTileList();
+        PlacePlayerOnTile();
+    }
+
+    private void GenerateTileList()
+    {
+        Transform[] allChildren = tilesHolder.GetComponentsInChildren<Transform>();
+        for (int i = 0; i < allChildren.Length; i++)
+        {
+            Transform child = allChildren[i];
+            if (child.CompareTag("Game Tile"))
+            {
+                tileList.Add(child);
+            }
+        }
+    }
+
     public void RollDice()
     {
         int totalRoll = 0;
@@ -12,6 +37,19 @@ public class DiceManager : MonoBehaviour
         {
             totalRoll += dice.Roll();
         }
-        //Debug.Log("Rolled: " + totalRoll);
+
+        curentTileIndex += totalRoll;
+        // make sure the current index does not go out of bounds
+        if(curentTileIndex>= tileList.Count)
+        {
+            curentTileIndex -= tileList.Count;
+        }
+        PlacePlayerOnTile(curentTileIndex);
+    }
+
+    private void PlacePlayerOnTile(int tileIndex = 0)
+    {
+        player.transform.position = tileList[tileIndex].transform.position;
+        player.transform.rotation = tileList[tileIndex].transform.rotation;
     }
 }
