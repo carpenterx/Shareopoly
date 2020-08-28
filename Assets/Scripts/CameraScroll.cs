a7bef59d71377b0c5070acedef0d88c812883b20
Sorin <jordache_sor@yahoo.com>
2020-08-28 12:44:01 +0300

    improved the camera scroll code

--- Assets/Scripts/CameraScroll.cs
+++ Assets/Scripts/CameraScroll.cs
@@ -7,6 +7,9 @@ public class CameraScroll : MonoBehaviour
     public int boundary = 50;
     public int speed  = 20;
 
+    public int horizontalOffset = 0;
+    public int verticalOffset = 0;
+
     private int theScreenWidth;
     private int theScreenHeight;
     private float startX;
@@ -18,10 +21,14 @@ public class CameraScroll : MonoBehaviour
     {
         theScreenWidth = Screen.width;
         theScreenHeight = Screen.height;
-        endX = transform.position.x;
+        startX = Mathf.Min(transform.position.x, transform.position.x + horizontalOffset);
+        endX = Mathf.Max(transform.position.x, transform.position.x + horizontalOffset);
+        startY = Mathf.Min(transform.position.y, transform.position.y + verticalOffset);
+        endY = Mathf.Max(transform.position.y, transform.position.y + verticalOffset);
+        /*endX = transform.position.x;
         startY = transform.position.y;
         startX = endX - 38;
-        endY = startY + 46;
+        endY = startY + 46;*/
     }
 
     void Update()
@@ -29,24 +36,30 @@ public class CameraScroll : MonoBehaviour
         if (!EventSystem.current.IsPointerOverGameObject())
         {
             Vector3 cameraPosition = transform.position;
-            if (Input.mousePosition.x > theScreenWidth - boundary)
+            if(horizontalOffset != 0)
             {
-                cameraPosition.x = Mathf.Min(endX, cameraPosition.x + (speed * Time.deltaTime));
-            }
+                if (Input.mousePosition.x > theScreenWidth - boundary)
+                {
+                    cameraPosition.x = Mathf.Min(endX, cameraPosition.x + (speed * Time.deltaTime));
+                }
 
-            if (Input.mousePosition.x < 0 + boundary)
-            {
-                cameraPosition.x = Mathf.Max(startX, cameraPosition.x - (speed * Time.deltaTime));
+                if (Input.mousePosition.x < 0 + boundary)
+                {
+                    cameraPosition.x = Mathf.Max(startX, cameraPosition.x - (speed * Time.deltaTime));
+                }
             }
-
-            if (Input.mousePosition.y > theScreenHeight - boundary)
+            
+            if(verticalOffset != 0)
             {
-                cameraPosition.y = Mathf.Min(endY, cameraPosition.y + (speed * Time.deltaTime));
-            }
+                if (Input.mousePosition.y > theScreenHeight - boundary)
+                {
+                    cameraPosition.y = Mathf.Min(endY, cameraPosition.y + (speed * Time.deltaTime));
+                }
 
-            if (Input.mousePosition.y < 0 + boundary)
-            {
-                cameraPosition.y = Mathf.Max(startY, cameraPosition.y - (speed * Time.deltaTime));
+                if (Input.mousePosition.y < 0 + boundary)
+                {
+                    cameraPosition.y = Mathf.Max(startY, cameraPosition.y - (speed * Time.deltaTime));
+                }
             }
 
             transform.position = cameraPosition;
