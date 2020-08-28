3deb9b6c7bd008a8e7d92ed33d61d3c453de641a
Sorin <jordache_sor@yahoo.com>
2020-08-28 17:48:44 +0300

    added dice (fixes #17)

---
+++ Assets/Scripts/DiceSprite.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class DiceSprite : MonoBehaviour
+{
+    public List<Sprite> faceSprites;
+
+    public SpriteRenderer spriteRenderer;
+
+    private void Start()
+    {
+        Roll();
+    }
+
+    private void OnMouseDown()
+    {
+        Roll();
+    }
+
+    public int Roll()
+    {
+        int dieIndex = Random.Range(0, faceSprites.Count);
+        spriteRenderer.sprite = faceSprites[dieIndex];
+        Quaternion randomRotation = Quaternion.Euler(0, 0, Random.Range(0, 360));
+        transform.rotation = randomRotation;
+        return dieIndex + 1;
+    }
+}
