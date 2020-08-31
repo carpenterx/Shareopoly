b66f19423501e0be6602364c1ba5b64f3283d0fe
Sorin <jordache_sor@yahoo.com>
2020-08-31 19:47:54 +0300

    added all yacht combinations

---
+++ Assets/Scripts/Combination.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class Combination : MonoBehaviour
+{
+    public CombinationScriptableObject combinationData;
+
+    public TextMesh nameMesh;
+    public List<SpriteRenderer> combinationDice;
+
+    void Start()
+    {
+        if (combinationData)
+        {
+            nameMesh.text = combinationData.combinationName;
+            for (int i = 0; i < combinationDice.Count; i++)
+            {
+                combinationDice[i].sprite = combinationData.diceSprites[i];
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/CombinationScriptableObject.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Combination", menuName = "ScriptableObjects/CombinationScriptableObject", order = 5)]
+public class CombinationScriptableObject : ScriptableObject
+{
+    public string combinationName;
+    public List<Sprite> diceSprites;
+}
