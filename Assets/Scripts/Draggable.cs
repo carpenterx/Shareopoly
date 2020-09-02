5e36bd01bc32f35b9c946d26622e9bd9adc571e6
Sorin <jordache_sor@yahoo.com>
2020-09-02 18:15:50 +0300

    added trackers

---
+++ Assets/Scripts/Draggable.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class Draggable : MonoBehaviour
+{
+    private bool isBeingDragged = false;
+    private Vector3 previousMousePosition;
+
+    private void OnMouseDown()
+    {
+        isBeingDragged = true;
+        previousMousePosition = GetWorldMousePosition();
+    }
+
+    private void OnMouseUp()
+    {
+        isBeingDragged = false;
+    }
+
+    private void OnMouseDrag()
+    {
+        if (isBeingDragged)
+        {
+            Vector3 currentMousePosition = GetWorldMousePosition();
+            transform.position += currentMousePosition - previousMousePosition;
+            previousMousePosition = currentMousePosition;
+        }
+    }
+
+    private Vector3 GetWorldMousePosition()
+    {
+        Vector3 mousePos = Input.mousePosition;
+        mousePos.z = 9;
+        Vector3 worldPosition = Camera.main.ScreenToWorldPoint(mousePos);
+        return worldPosition;
+    }
+}
